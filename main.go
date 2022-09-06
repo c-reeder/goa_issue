dda974ee4a35f8df0f713391b88092931d5d8c27
Connor Reeder <connor.reeder@ymail.com>
2022-09-06 07:52:02 -0400

    bumped to 3.8.5 and issue is confirmed fixed

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 
 	_ "goa.design/goa/v3/codegen/generator"
@@ -17,15 +16,25 @@ var _ testservice.Service = (*Service)(nil)
 type Service struct {
 }
 
-// Update implements testservice.Service
-func (*Service) Update(ctx context.Context, payload *testservice.Workspace) (err error) {
-	return fmt.Errorf("Incoming ID: %s", payload.ID)
+// Set implements testservice.Service
+func (*Service) Set(context.Context, *testservice.Workspace) (err error) {
+	panic("unimplemented")
 }
 
-// Set implements testservice.Service
-func (*Service) Set(ctx context.Context, payload *testservice.Workspace) (err error) {
-	return fmt.Errorf("Incoming ID: %s", payload.ID)
+// Update implements testservice.Service
+func (*Service) Update(context.Context, *testservice.WorkspaceUpdatePayload) (err error) {
+	panic("unimplemented")
 }
+
+//Update implements testservice.Service
+//func (*Service) Update(ctx context.Context, payload *testservice.Workspace) (err error) {
+//return fmt.Errorf("Incoming ID: %s", payload.ID)
+//}
+
+//Set implements testservice.Service
+//func (*Service) Set(ctx context.Context, payload *testservice.Workspace) (err error) {
+//return fmt.Errorf("Incoming ID: %s", payload.ID)
+//}
 
 func main() {
 	s := &Service{}
